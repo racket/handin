c9b9035f1f3f736153255467243fe18e0a652340
Matthew Flatt <mflatt@racket-lang.org>
2014-07-09 08:32:10 +0100

    declare package dependencies

--- info.rkt
+++ info.rkt
@@ -1,3 +1,15 @@
 #lang setup/infotab
 
 (define collection 'multi)
+
+(define deps '("base"
+               "compatibility-lib"
+               "drracket"
+               "drracket-plugin-lib"
+               "gui-lib"
+               "htdp-lib"
+               "net-lib"
+               "pconvert-lib"
+               "sandbox-lib"
+               "web-server-lib"))
+(define build-deps '("scribble-lib"))
